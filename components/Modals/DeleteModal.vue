66893a4343c68c14c9dd501fe4d9f93e8caad3b3
SenghongH <senghong.hang@techbodia.com>
2025-07-19 12:31:55 +0700

    Update modal

--- components/Modals/DeleteModal.vue
+++ components/Modals/DeleteModal.vue
@@ -1,7 +1,8 @@
 <template>
-    <div v-if="modal" class="fixed inset-0 w-full h-[100dvh] z-10 bg-black/10" @click="$emit('close')">
-        <div class="absolute bottom-0 left-0 right-0 z-20 bg-gray-600/60 p-4 grid gap-4 rounded-t-xl" role="dialog"
-            aria-modal="true" @click.stop @touchstart.stop>
+    <div v-if="modal" class="fixed inset-0 w-full h-[100dvh] z-10 bg-black/10" @click="$emit('close')" role="dialog"
+        aria-modal="true">
+        <div class="absolute bottom-0 left-0 right-0 z-20 bg-gray-600/60 p-4 grid gap-4 rounded-t-xl" @click.stop
+            @touchstart.stop aria-labelledby="dialog-title">
             <Button1 @click="handleDelete" name="OK" type="danger" :loading="isLoading" />
             <Button1 @click="$emit('close')" name="Cancel" type="info" />
         </div>
